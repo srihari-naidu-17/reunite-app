93935c51613558c0e6ebb7b9d5083fa32dd9e460
iimm-david <1211303395@soffice.mmu.edu.my>
2024-07-01 22:36:10 +0800

    Added Profile Fragment

--- app/src/main/java/com/example/reuniteapp/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/reuniteapp/ui/profile/ProfileFragment.kt
@@ -1,4 +1,4 @@
-package com.example.reuniteapp.ui.dashboard
+package com.example.reuniteapp.ui.profile
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.reuniteapp.databinding.FragmentDashboardBinding
+import com.example.reuniteapp.databinding.FragmentProfileBinding
 
-class DashboardFragment : Fragment() {
+class ProfileFragment : Fragment() {
 
-    private var _binding: FragmentDashboardBinding? = null
+    private var _binding: FragmentProfileBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -22,14 +22,14 @@ class DashboardFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val dashboardViewModel =
-            ViewModelProvider(this)[DashboardViewModel::class.java]
+        val profileViewModel =
+            ViewModelProvider(this)[ProfileViewModel::class.java]
 
-        _binding = FragmentDashboardBinding.inflate(inflater, container, false)
+        _binding = FragmentProfileBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textDashboard
-        dashboardViewModel.text.observe(viewLifecycleOwner) {
+        val textView: TextView = binding.textProfile
+        profileViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
         return root
