9096b7177d3cadbad0c946d131db577153230a7c
Shravan Ram V <shravanv.ram@gmail.com>
2024-07-02 16:14:59 +0800

    Updated about page

--- app/src/main/java/com/example/reuniteapp/MainActivity.kt
+++ app/src/main/java/com/example/reuniteapp/MainActivity.kt
@@ -55,7 +55,8 @@ class MainActivity : AppCompatActivity() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
             R.id.action_about -> {
-                // Handle about action
+                val intent = Intent(this, AboutActivity::class.java)
+                startActivity(intent)
                 true
             }
             R.id.action_logout -> {
